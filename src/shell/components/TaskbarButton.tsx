dd87d9fd8eee26ad9e5612fa193e585cfda4cc00
Tatu Arvela <tatu.arvela@nitor.com>
2020-12-11 19:18:01 +0200

    Improve default Taskbar buttons

--- src/shell/components/TaskbarButton.tsx
+++ src/shell/components/TaskbarButton.tsx
@@ -1,15 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 type TaskbarButtonProps = {
   active: boolean;
 };
+const activeStyle = css`
+  font-weight: bold;
+  border-color: lightgray;
+  background: none;
+  box-shadow: inset 1px 1px 0 gray;
+`;
 const TaskbarButton = styled.button<TaskbarButtonProps>`
-  filter: ${(props) => !props.active && 'brightness(80%)'};
-  font-weight: ${(props) => props.active && 'bold'};
   height: 22px;
   margin-left: 2px;
   text-align: left;
   width: 160px;
+  border: 1px solid gray;
+  border-radius: 3px;
+  box-shadow: 1px 1px 0 lightgray;
+  ${(props) => props.active && activeStyle};
 `;
 TaskbarButton.defaultProps = {
   type: 'button',
