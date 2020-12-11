43005236521088e907f50012e2609da25a20c291
Tatu Arvela <tatu.arvela@nitor.com>
2020-12-12 00:04:17 +0200

    Adjust ResizeBorder sizing and order

--- src/window/components/ResizeBorder.tsx
+++ src/window/components/ResizeBorder.tsx
@@ -23,8 +23,8 @@ const NEResize = styled.div`
   position: absolute;
   top: ${offset}px;
   right: ${offset}px;
-  height: ${size}px;
-  width: ${size}px;
+  height: ${size * 2}px;
+  width: ${size * 2}px;
   cursor: ne-resize;
 `;
 
@@ -41,8 +41,8 @@ const SEResize = styled.div`
   position: absolute;
   bottom: ${offset}px;
   right: ${offset}px;
-  height: ${size}px;
-  width: ${size}px;
+  height: ${size * 2}px;
+  width: ${size * 2}px;
   cursor: se-resize;
 `;
 
@@ -59,8 +59,8 @@ const SWResize = styled.div`
   position: absolute;
   bottom: ${offset}px;
   left: ${offset}px;
-  height: ${size}px;
-  width: ${size}px;
+  height: ${size * 2}px;
+  width: ${size * 2}px;
   cursor: sw-resize;
 `;
 
@@ -77,20 +77,20 @@ const NWResize = styled.div`
   position: absolute;
   top: ${offset}px;
   left: ${offset}px;
-  height: ${size}px;
-  width: ${size}px;
+  height: ${size * 2}px;
+  width: ${size * 2}px;
   cursor: nw-resize;
 `;
 
 const ResizeBorder = ({ resize }: ResizeBorderProps): JSX.Element => (
   <>
     <NResize onMouseDown={(e) => resize(e, Direction.N)} />
-    <NEResize onMouseDown={(e) => resize(e, Direction.NE)} />
     <EResize onMouseDown={(e) => resize(e, Direction.E)} />
-    <SEResize onMouseDown={(e) => resize(e, Direction.SE)} />
     <SResize onMouseDown={(e) => resize(e, Direction.S)} />
-    <SWResize onMouseDown={(e) => resize(e, Direction.SW)} />
     <WResize onMouseDown={(e) => resize(e, Direction.W)} />
+    <NEResize onMouseDown={(e) => resize(e, Direction.NE)} />
+    <SEResize onMouseDown={(e) => resize(e, Direction.SE)} />
+    <SWResize onMouseDown={(e) => resize(e, Direction.SW)} />
     <NWResize onMouseDown={(e) => resize(e, Direction.NW)} />
   </>
 );
