9e40112812b378584b8fcdf1a632c427a1785527
Tatu Arvela <tatu.arvela@nitor.com>
2021-11-22 11:19:32 +0200

    Upgrade dependencies and simplify ESLint config

--- src/windowManager/WindowManagerContext.tsx
+++ src/windowManager/WindowManagerContext.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 import { WindowManager } from './types';
 
-export const WindowManagerContext = React.createContext<WindowManager>(
-  undefined
-);
+export const WindowManagerContext =
+  React.createContext<WindowManager>(undefined);
 WindowManagerContext.displayName = 'WindowManagerContext';
 
 export const WindowManagerProvider = WindowManagerContext.Provider;
