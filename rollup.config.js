c70c1b4c2fe8f5a1a2ae6a8f75070daf2c279fe9
Tatu Arvela <tatu.arvela@nitor.com>
2021-11-25 20:25:01 +0200

    Improve rollup configuration to export types

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import del from 'rollup-plugin-delete';
+import dts from 'rollup-plugin-dts';
 import externals from 'rollup-plugin-node-externals';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
 export default [
   {
     input: './src/index.ts',
-    external: [/^react/],
     plugins: [
       // Delete existing build files.
       del({ targets: 'dist/*' }),
@@ -21,16 +23,24 @@ export default [
       }),
       // Convert CommonJS modules into ES modules.
       commonjs(),
+      // Generate types.
+      typescript({ tsconfig: './tsconfig.json' }),
       // Compile the library's code into a format that is consumable by a wider set of browsers. The library's code lives inside `.js`, `.jsx`, `.ts` and `.tsx` files. Do not compile any files from `node_modules`. The `runtime` helper makes Babel's injected helper code reusable for all modules, which greatly reduces bundle size.
       babel({
         babelHelpers: 'runtime',
-        exclude: '**/node_modules/**',
+        exclude: 'node_modules/**',
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       }),
+      terser({ format: { comments: false } }),
     ],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
     ],
+  },
+  {
+    input: './dist/dts/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts(), del({ hook: 'buildEnd', targets: './dist/dts' })],
   },
 ];
