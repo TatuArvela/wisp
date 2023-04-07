569ef3abd63a77b833ab30a0099fb1dcf16c6907
Tatu Arvela <tatu.arvela@nitor.com>
2023-04-08 02:35:47 +0300

    Use ComboBox in ThemeSwitcher

--- src/elements/ThemeSwitcher.tsx
+++ src/elements/ThemeSwitcher.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 
+import ComboBox from '../controls/ComboBox';
 import { useThemeManager } from '../themeManager/hooks';
 import Window from '../window/Window';
 
 const ThemeSwitcher = () => {
   const { theme: currentTheme, themes, changeTheme } = useThemeManager();
-  const onChange = (e) => {
-    const newTheme = themes.find((theme) => theme.id === e.target.value);
+
+  const value = `${currentTheme.name} (Active)`;
+
+  const options = themes.map(
+    (theme) => `${theme.name}${theme.id === currentTheme.id ? ' (Active)' : ''}`
+  );
+
+  const onChange = (value: string) => {
+    const newTheme = themes.find((theme) => theme.name === value);
     newTheme && changeTheme(newTheme);
   };
 
   return (
-    <Window initialState={{ title: 'Theme Switcher' }} id="THEME_SWITCHER">
-      <select onChange={onChange} value={currentTheme.id}>
-        {themes.map((theme) => (
-          <option key={theme.id} value={theme.id}>
-            {theme.name}
-            {theme.id === currentTheme.id && ' (Active)'}
-          </option>
-        ))}
-      </select>
+    <Window
+      initialState={{
+        title: 'Theme Switcher',
+        isResizable: false,
+        isClosable: false,
+        isMaximizable: false,
+        height: 58,
+      }}
+      id="THEME_SWITCHER"
+    >
+      <ComboBox onChange={onChange} value={value} options={options} />
     </Window>
   );
 };
