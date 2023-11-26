4440e097a345c69c6ccf8fd46f103f1f3cbc0cce
Tatu Arvela <tatu.arvela@nitor.com>
2023-11-26 16:41:15 +0200

    upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,13 @@ import type { StorybookConfig} from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.tsx'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
+
   typescript: {
     check: false,
     checkOptions: {},
@@ -16,14 +18,18 @@ const config: StorybookConfig = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
+
   core: {
     disableTelemetry: true,
   },
+
   staticDirs: ['../stories/assets'],
+
   env: (config) => ({...config, STORYBOOK_PACKAGE_VERSION: process.env.npm_package_version! }),
 };
 export default config;
