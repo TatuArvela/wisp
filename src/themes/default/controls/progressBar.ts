d154f6776797680bd559d3b84bba517fc0114634
Tatu Arvela <tatu.arvela@nitor.com>
2024-03-15 13:23:32 +0200

    rename progress bar indeterminate style animation

--- src/themes/default/controls/progressBar.ts
+++ src/themes/default/controls/progressBar.ts
@@ -24,7 +24,7 @@ export const ProgressBarFill = css<ProgressBarFillProps>`
     props.isIndeterminate ? (props.disabled ? '0%' : '50%') : 'var(--width)'};
   ${(props) => !props.disabled && props.isIndeterminate && indeterminateStyle};
 
-  @keyframes indeterminate-progress-bar-fill-animation {
+  @keyframes wisp-indeterminate-progress-bar-fill-animation {
     0% {
       transform: translateX(-100%);
     }
@@ -36,7 +36,7 @@ export const ProgressBarFill = css<ProgressBarFillProps>`
 
 const indeterminateStyle = css`
   animation-duration: 1s;
-  animation-name: indeterminate-progress-bar-fill-animation;
+  animation-name: wisp-indeterminate-progress-bar-fill-animation;
   animation-iteration-count: infinite;
   animation-timing-function: linear;
 `;
