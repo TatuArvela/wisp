48d04dd08a0561d0400ae684ec3fc2ccf54dbcbe
Tatu Arvela <tatu.arvela@nitor.com>
2024-09-14 01:17:53 +0300

    fix refit bug

--- src/window/handlers/refitWindow.ts
+++ src/window/handlers/refitWindow.ts
@@ -10,6 +10,10 @@ const refitWindow = (
   window: WindowType,
   boundaries: Boundaries
 ): WindowType => {
+  if (!window.height || !window.width) {
+    return window;
+  }
+
   const positionX = calculatePositionX(window, 0, boundaries);
   const positionY = calculatePositionY(window, 0, boundaries);
 
